799084f2d88f0bfe892a27ab936889e5668acf55
Tommy Stuart <tommystuart@MacBook-Air-2.local>
2024-06-20 09:58:13 +0100

    so Ive gone back to gnosis contracts. I also think I need to spend some time doing the tutorial with them pre integrating here. Going to create a new repo in 100-days-of-code to do this tutorial. Will leave daily notes there for now and come back here when done - https://docs.gnosis.io/conditionaltokens/docs/pmtutorial3

---
+++ contracts/AppDependencies.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.1;
+
+// NOTE: This file porpouse is just to make sure truffle compiles all of depending
+//  contracts when we are in development.
+
+import './FixedProductMarketMaker.sol';
+import './FixedProductMarketMaker.sol';
+
+contract AppDependencies {
+}
